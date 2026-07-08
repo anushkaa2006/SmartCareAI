50aa0e65e262df388d509c4e328d576a0396325c
Anushka Goel <anushkagoel273@gmail.com>
2026-07-08 12:40:58 +0530

    changes

--- backend/src/main/java/com/smartcare/repository/DepartmentRepository.java
+++ backend/src/main/java/com/smartcare/repository/DepartmentRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository
-        extends JpaRepository<Department,String> {
+public interface DepartmentRepository extends JpaRepository<Department,String> {
 
 Department findByDepartmentName(String departmentName);
         }
